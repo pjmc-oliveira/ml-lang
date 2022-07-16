9af6b71f6a0f9f5cafb4202050c3c46403ec3f59
Pedro Oliveira <pjmc.oliveira@gmail.com>
2022-07-16 19:24:54 -0400

    Cleaned up mail.ml

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,7 @@ open Result.Syntax
 let run str =
   let src = Source.of_string str in
   let* tks = Lexer.tokens src in
-  let* m, _tks' = Parser.parse Parser.module_ tks in
+  let* m, _tks' = Parser.(parse module_ tks) in
   Ok m
 
 let report res =
@@ -15,6 +15,6 @@ let report res =
       "Error:\n" ^ lines
   | Ok m -> "Ok:\n" ^ Ast.show_module_ m
 
-let source = "module Hello = { def hello = 1 def bye = 2 }"
+let source = "\nmodule Hello = {\n  def hello = 1 def bye = 2\n}\n"
 let () = print_string (report (run source))
 (* let () = prerr_string "Hello, World!" *)
