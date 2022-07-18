0cdf5fcb503941490c5bb0948689fc7fb9224e3f
Pedro Oliveira <pjmc.oliveira@gmail.com>
2022-07-18 12:26:52 -0400

    Added very temporary file reading function

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,20 @@
 open Ml_lang
 open Result.Syntax
 
+(* TODO: make this lazy? *)
+let read_lines path : string =
+  let ic = open_in (String.concat Filename.dir_sep path) in
+  let try_read () = try Some (input_line ic) with End_of_file -> None in
+  let rec loop lines =
+    match try_read () with
+    | Some line -> loop (line :: lines)
+    | None ->
+        close_in ic;
+        List.rev lines
+  in
+  let lines = loop [] in
+  String.concat "\n" lines
+
 let run str =
   let src = Source.of_string str in
   let* tks = Lexer.tokens src in
@@ -16,14 +30,11 @@ let report res =
       "Failure:\n" ^ lines
   | Ok m -> "Success:\n" ^ Tast.Module.show m
 
-let source = "
-module Hello = {
-  def hello = 1
-  def bye = 2
-}"
+let source = read_lines [ "examples"; "hello.luz" ]
 
 (* TODO *)
 (* let source = "\nmodule Hello = {\n  def = 1 def = 1\n  def bye = 2\n}\n" *)
 
 (* let source = "\nmodule Hello = {}\n" *)
 let () = print_string (report (run source))
+(* let () = Sys.argv |> Array.to_list |> String.concat ", " |> print_string *)
